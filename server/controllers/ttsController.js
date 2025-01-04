f2a22b13152407339a4e27070d661df455a0c6fa
David Stein <dstein131@gmail.com>
2025-01-03 20:35:48 -0500

    toss

--- server/controllers/ttsController.js
+++ server/controllers/ttsController.js
@@ -177,6 +177,7 @@ const getAvailableVoicesController = async (req, res) => {
   try {
     const voices = [
       { id: '2auXDq7PlBLK7DMRBihn', name: 'Toss Pot' },
+      { id: 'e2HVoFYmKNmDxY49SwxX', name: 'Bubblegum' },
       { id: '5OkdfFcTx8GPEM1HJilp', name: 'GGG' },
       { id: 'P7JLNjmKIOM2sSzz616k', name: 'Becky' },
       { id: 'sDSBbBr0YrSbCDpcIuFZ', name: 'Joey C' },
